64218742f5e20a16d2380ed9345843747ff626c8
Robin <gejw0623@yeah.net>
2013-12-22 19:12:13 +0800

    Initial Commit

---
+++ TextFiledWithKeyBoard/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TextFiledWithKeyBoard
+//
+//  Created by Robin on 13-12-22.
+//  Copyright (c) 2013年 gejw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TextFiledWithKeyBoard/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TextFiledWithKeyBoard
+//
+//  Created by Robin on 13-12-22.
+//  Copyright (c) 2013年 gejw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
